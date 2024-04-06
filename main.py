b1be4193f6719276eb528e460abc17339941c5a2
Kaloyan Peychev <kokoeverest@gmail.com>
2024-04-06 20:32:21 +0300

    Refactor the file structure and add functionality
    Add options to add a single Source or a collection of Source. Check added to verify that there is at least one source in order to call the execute method, otherwise a ValueError is raised

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from time_lapse_creator import Source, TimeLapseCreator
+from app.time_lapse_creator import Source, TimeLapseCreator
 
-sources_list: list[Source] = [
-    Source("aleko", "https://home-solutions.bg/cams/aleko2.jpg?1705293967111"),
-    Source("markudjik", "https://media.borovets-bg.com/cams/channel?channel=31"),
-    Source("plevenhut", "https://meter.ac/gs/nodes/N160/snap.jpg?1705436803718"),
-]
+
+aleko_source = Source(
+    "aleko", "https://home-solutions.bg/cams/aleko2.jpg?1705293967111"
+)
+borovets_source = Source(
+    "markudjik", "https://media.borovets-bg.com/cams/channel?channel=31"
+)
+pleven_hut_source = Source(
+    "plevenhut", "https://meter.ac/gs/nodes/N160/snap.jpg?1705436803718"
+)
+
+sources_list: list[Source] = [aleko_source, borovets_source, pleven_hut_source]
 
 bulgaria_webcams = TimeLapseCreator(sources_list)
 
