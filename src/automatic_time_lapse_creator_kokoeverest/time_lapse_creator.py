075a64f460f5949934f92270294c62433ef3c4a7
Kaloyan Peychev <kokoeverest@gmail.com>
2024-11-20 06:51:10 +0200

    Refactor: the logic for partially collected images during the day, according to the new structure of the Source class

--- src/automatic_time_lapse_creator_kokoeverest/time_lapse_creator.py
+++ src/automatic_time_lapse_creator_kokoeverest/time_lapse_creator.py
@@ -2,24 +2,17 @@ import os
 import requests
 from datetime import datetime as dt
 from time import sleep
-from typing import NamedTuple
 from pathlib import Path
 from typing import Iterable
 import logging
+from src.automatic_time_lapse_creator_kokoeverest.source import Source
 from src.automatic_time_lapse_creator_kokoeverest.video_manager import (
     VideoManager as vm,
 )
 from src.automatic_time_lapse_creator_kokoeverest.time_manager import (
     LocationAndTimeManager,
 )
-from src.automatic_time_lapse_creator_kokoeverest.common.constants import (
-    JPG_FILE,
-    MP4_FILE,
-    LOG_FILE,
-    YYMMDD_FORMAT,
-    HHMMSS_UNDERSCORE_FORMAT,
-    HHMMSS_COLON_FORMAT,
-)
+from src.automatic_time_lapse_creator_kokoeverest.common.constants import *
 from src.automatic_time_lapse_creator_kokoeverest.common.exceptions import (
     InvalidStatusCodeException,
     InvalidCollectionException,
@@ -45,22 +38,6 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
 
-class Source(NamedTuple):
-    """The Source class contains two attributes:
-
-    Attributes::
-
-        location_name: str - a folder with that name will be created on your pc. The videos
-            for every day of the execution of the TimeLapseCreator will be created and put into
-            subfolders into the "location_name" folder
-
-        url: str - a valid web address where a webcam frame (image) should be located.
-    Be sure that the url does not point to a video resource."""
-
-    location_name: str
-    url: str
-
-
 class TimeLapseCreator:
     """"""
 
@@ -80,48 +57,49 @@ class TimeLapseCreator:
         self.location = LocationAndTimeManager(city)
         self.sources: set[Source] = set(sources)
         self.wait_before_next_frame = seconds_between_frames
-        self.wait_before_next_retry = night_time_retry_seconds
+        self.nighttime_wait_before_next_retry = night_time_retry_seconds
         self.video_fps = video_fps
         self.video_width = video_width
         self.video_height = video_height
 
     def execute(self):
-        """"""
+        """If the source.all_images_collected is True
+        Don't delete the source images if there's a
+        problem with the output video"""
 
         self.verify_sources_not_empty()
         while True:
-            images_collected = self.collect_images_from_webcams()
+            # images_collected, images_partially_collected = (
+            self.collect_images_from_webcams()
+            # )
 
-            if dt.now() > self.location.end_of_daylight and images_collected:
-                for source in self.sources:
-                    input_folder = (
-                        f"{self.base_path}/{source.location_name}/{self.folder_name}"
-                    )
-                    output_video = str(
-                        Path(f"{input_folder}/{self.folder_name}{MP4_FILE}")
-                    )
-
-                    if not vm.video_exists(output_video):
-                        created = vm.create_timelapse(
-                            input_folder,
-                            output_video,
-                            self.video_fps,
-                            self.video_width,
-                            self.video_height,
-                        )
-                    else:
-                        created = False
-
-                    if created:
-                        _ = vm.delete_source_images(input_folder)
+            for source in self.sources:
+                # the normal flow of images collection, when all images are collected during the day
+                if (
+                    dt.now() > self.location.end_of_daylight
+                    and source.images_collected
+                    and not source.images_partially_collected
+                    and not source.video_created
+                ):
+                    self.create_video(source)
+                # if there was an interruption in program's execution but some images were collected
+                # create a video anyway, but don't delete the source images
+                elif (
+                    dt.now() > self.location.end_of_daylight
+                    and source.images_partially_collected
+                    and not source.images_collected
+                    and not source.video_created
+                ):
+                    self.create_video(source, delete_source_images=False)
             else:
-                logger.info("Not daylight yet")
-                sleep(self.wait_before_next_retry)
+                sleep(self.nighttime_wait_before_next_retry)
 
-    def collect_images_from_webcams(self) -> bool:
+    def collect_images_from_webcams(self) -> None:
         """"""
+        # images_collected, images_partially_collected = False, False
 
         if self.location.is_daylight():
+            self.reset_sources_image_counters_and_video_created()
             logger.info("Start collecting images")
 
             while self.location.is_daylight():
@@ -129,9 +107,8 @@ class TimeLapseCreator:
                     try:
                         img = self.verify_request(source)
 
-                        location = source.location_name
                         file_name = dt.now().strftime(HHMMSS_UNDERSCORE_FORMAT)
-                        current_path = f"{self.base_path}/{location}/{self.folder_name}"
+                        current_path = f"{self.base_path}/{source.location_name}/{self.folder_name}"
 
                         Path(current_path).mkdir(parents=True, exist_ok=True)
                         if img:
@@ -139,15 +116,53 @@ class TimeLapseCreator:
 
                             with open(full_path, "wb") as file:
                                 file.write(img)
+                            source.increase_images()
+                            source.set_images_partially_collected()
+
                     except Exception as e:
                         logger.error(e)
                         continue
                 sleep(self.wait_before_next_frame)
 
+            self.set_sources_all_images_collected()
+            self.reset_images_partially_collected()
+            # images_collected, images_partially_collected = True, False
             logger.info(f"Finished collecting for today")
-            return True
 
-        return False
+            # return images_collected, images_partially_collected
+        else:
+            logger.info("Not daylight yet")
+
+        # return images_collected, images_partially_collected
+
+    def create_video(self, source: Source, delete_source_images: bool = True):
+        """Creates a video from the source collected images. If delete_source_images is True
+        the source image files will be deleted after the video is created
+
+        Attributes::
+
+            source: Source - the source from which collected images the video will be created
+
+            delete_source_images: bool - if the source images should be deleted as well"""
+
+        # for source in self.sources:
+        input_folder = f"{self.base_path}/{source.location_name}/{self.folder_name}"
+        output_video = str(Path(f"{input_folder}/{self.folder_name}{MP4_FILE}"))
+
+        created = False
+        if not vm.video_exists(output_video):
+            created = vm.create_timelapse(
+                input_folder,
+                output_video,
+                self.video_fps,
+                self.video_width,
+                self.video_height,
+            )
+        if created:
+            source.set_video_created()
+
+        if source.video_created and delete_source_images:
+            _ = vm.delete_source_images(input_folder)
 
     def verify_sources_not_empty(self):
         """Verifies that TimeLapseCreator has at least one Source to take images for.
@@ -172,14 +187,27 @@ class TimeLapseCreator:
 
         try:
             response = requests.get(source.url)
-            if response.status_code != 200:
+            if response.status_code != OK_STATUS_CODE:
                 raise InvalidStatusCodeException(
-                    f"Status code {response.status_code} is not 200 for url {source}"
+                    f"Status code {response.status_code} is not {OK_STATUS_CODE} for url {source}"
                 )
         except Exception as exc:
             raise exc
 
         return response.content
+
+    def reset_images_partially_collected(self):
+        """Resets the _images_partially_collected = False for all self.sources"""
+        (source.reset_images_pertially_collected() for source in self.sources)
+
+    def reset_sources_image_counters_and_video_created(self):
+        """Resets the images_count and resets video_created = False for all self.sources"""
+        (source.reset_images_counter() for source in self.sources)
+        (source.reset_video_created() for source in self.sources)
+
+    def set_sources_all_images_collected(self):
+        """Sets -> _all_images_collected = True for all self.sources"""
+        (source.set_all_images_collected() for source in self.sources)
 
     def add_sources(self, sources: Source | Iterable[Source]):
         """Adds a single Source or a collection[Source] to the TimeLapseCreator sources.
