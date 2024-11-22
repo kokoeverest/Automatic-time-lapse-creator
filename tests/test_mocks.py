f54e1a894bb3e878efc340516b51bc77778f5621
Kaloyan Peychev <kokoeverest@gmail.com>
2024-11-22 22:07:31 +0200

    Add more tests

--- tests/test_mocks.py
+++ tests/test_mocks.py
@@ -1,4 +1,30 @@
 from src.automatic_time_lapse_creator_kokoeverest.common.constants import *
+from datetime import datetime
+from unittest.mock import Mock
+from cv2.typing import MatLike
+
+today = datetime.today()
+
+def mock_jpg_file(number: int=1):
+    mock_file = Mock()
+    mock_file.name = f"test_image_{number}.jpg"
+    mock_file.read.return_value = b"fake image data"
+    return mock_file.name
+
+
+def mock_mat_like():
+    mat_like = Mock(spec=MatLike)
+
+    return mat_like
+
+mock_image = mock_jpg_file()
+mock_images_list = [mock_jpg_file(x) for x in range(1, 11)]
+mock_MatLike = mock_mat_like()
+mock_path_to_images_folder = "fake/folder/path"
+mock_output_video_name = "fake_video.mp4"
+mock_video_frames_per_second = 30
+mock_video_width = 640
+mock_video_height = 360
 
 
 class MockResponse:
@@ -13,3 +39,13 @@ class MockIsDaylight:
     @classmethod
     def true_return(cls):
         return True
+
+
+class MockDatetime:
+    fake_daylight = datetime(today.year, today.month, today.day, 12, 00, 00)
+    fake_nighttime = datetime(today.year, today.month, today.day, 00, 00, 00)
+
+
+class MockTimeAndLocationManager:
+    start_of_daylight = datetime(today.year, today.month, today.day, 7, 00, 00)
+    end_of_daylight = datetime(today.year, today.month, today.day, 18, 00, 00)
--- tests/test_time_manager.py
+++ tests/test_time_manager.py
@@ -1,8 +1,13 @@
+from datetime import datetime
 import pytest
-from src.automatic_time_lapse_creator_kokoeverest.time_manager import LocationAndTimeManager
-from src.automatic_time_lapse_creator_kokoeverest.common.exceptions import UnknownLocationException
+from astral import LocationInfo
+from src.automatic_time_lapse_creator_kokoeverest.time_manager import (
+    LocationAndTimeManager,
+)
+from src.automatic_time_lapse_creator_kokoeverest.common.exceptions import (
+    UnknownLocationException,
+)
 import tests.test_data as td
-
 
 
 @pytest.fixture
@@ -13,3 +18,53 @@ def sample_LocationAndTimeManager():
 def test_LocationAndTimeManager_raises_UnknownLocationException_if_city_is_not_found():
     with pytest.raises(UnknownLocationException):
         LocationAndTimeManager(td.invalid_city_name)
+
+
+def test_LocationAndTimeManager_initializes_correctly_for_correct_location(
+    sample_LocationAndTimeManager: LocationAndTimeManager,
+):
+    assert isinstance(sample_LocationAndTimeManager, LocationAndTimeManager)
+    assert isinstance(sample_LocationAndTimeManager.db, dict)
+    assert isinstance(sample_LocationAndTimeManager.city_is_location_info_object, bool)
+    assert isinstance(sample_LocationAndTimeManager.city, LocationInfo)
+
+    for attr in (
+        sample_LocationAndTimeManager.start_hour,
+        sample_LocationAndTimeManager.start_minutes,
+        sample_LocationAndTimeManager.end_hour,
+        sample_LocationAndTimeManager.end_minutes,
+    ):
+        assert isinstance(attr, int)
+
+    for attr in (
+        sample_LocationAndTimeManager.start_of_daylight,
+        sample_LocationAndTimeManager.end_of_daylight,
+    ):
+        assert isinstance(attr, datetime)
+
+
+def test_s_rise_returns_tuple_with_two_integers(
+    sample_LocationAndTimeManager: LocationAndTimeManager,
+):
+    actual_result = sample_LocationAndTimeManager.s_rise()
+
+    assert isinstance(actual_result, tuple)
+    assert len(actual_result) == 2
+    assert all(isinstance(x, int) for x in actual_result)
+
+
+def test_s_set_returns_tuple_with_two_integers(
+    sample_LocationAndTimeManager: LocationAndTimeManager,
+):
+    actual_result = sample_LocationAndTimeManager.s_set()
+
+    assert isinstance(actual_result, tuple)
+    assert len(actual_result) == 2
+    assert all(isinstance(x, int) for x in actual_result)
+
+
+def test_is_daylight_returns_True_during_the_day(
+    sample_LocationAndTimeManager: LocationAndTimeManager,
+):
+    result = sample_LocationAndTimeManager.is_daylight()
+    assert isinstance(result, bool)
